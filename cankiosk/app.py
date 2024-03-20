36614f344bedb4148f5b9c104c57e1fa0329017c
Zain Javaid <zainmfj@gmail.com>
2024-03-20 02:45:20 -0500

    Added routes for each role

--- cankiosk/app.py
+++ cankiosk/app.py
@@ -7,5 +7,20 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+# Judge experience routes
+@app.route('/judge')
+def judge_root():
+    return render_template('judge.html')
+
+# Scout experience routes
+@app.route('/scout')
+def scout_root():
+    return render_template('scout.html')
+
+# Guest experience routes
+@app.route('/guest')
+def guest_root():
+    return render_template('guest.html')
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=3000, debug=True)
