aed38de2d52672bc0f557b3f5f13895c0c058b3d
Zain Javaid <zainmfj@gmail.com>
2024-03-20 10:11:35 -0500

    Added routes for scout categories

--- cankiosk/app.py
+++ cankiosk/app.py
@@ -17,6 +17,22 @@ def judge_root():
 def scout_root():
     return render_template('scout.html')
 
+@app.route('/scout/specs')
+def scout_specs():
+    return render_template('scout/specs.html')
+
+@app.route('/scout/strategy')
+def scout_strategy():
+    return render_template('scout/strategy.html')
+
+@app.route('/scout/auton')
+def scout_auton():
+    return render_template('scout/auton.html')
+
+@app.route('/scout/documentation')
+def scout_documentation():
+    return render_template('scout/documentation.html')
+
 # Guest experience routes
 @app.route('/guest')
 def guest_root():
